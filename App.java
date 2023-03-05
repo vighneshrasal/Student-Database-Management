3b0414e8477092a0e607b3750e209bd4f4540543
Vighnesh Rasal <56250868+vighneshrasal@users.noreply.github.com>
2023-03-05 16:30:05 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args){
+        Student stul = new Student();
+    //Ask how many neew students we want to add
+    // Create n number of new students
+    }
+}
